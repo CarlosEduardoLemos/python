3e53423b10f64ee000b9cada742e82cda5c41ac2
Carlos Eduardo De Souza Lemos <92192426+CarlosEduardoLemos@users.noreply.github.com>
2024-11-04 15:11:48 -0300

    Update teste.py

--- Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex107/teste.py
+++ Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex107/teste.py
@@ -1,3 +1,6 @@
 import moeda
+
 p = float(input('Digite o preço: R$'))
-print(f'A metade de {p} é {moeda.metade(p)}')
+print(f'A metade de R${p} é R${moeda.metade(p)}')
+print(f'O dobro de R${p} é R${moeda.dobro(p)}')
+print(f'Aumentando 10%, temos R${moeda.aumentar(p, 10)}')
