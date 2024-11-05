289a0c806b260222357171550ec1fe710b8d6dbc
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-11-04 21:08:22 -0300

    Update teste.py

--- Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex112/teste.py
+++ Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex112/teste.py
@@ -1,5 +1,5 @@
 from utilidadescev import moeda
-from utilidadescev import dado 
+from utilidadescev import dado
 
-p = dado.leiaDinheiro('Digite o preço: R$'))
+p = dado.leiaDinheiro('Digite o preço: R$')
 moeda.resumo(p, 35, 22)
