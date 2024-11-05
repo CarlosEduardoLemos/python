fc08983e59633479e3d742eecc9dfae514ae7fe6
Carlos Eduardo De Souza Lemos <lemosecarlos@gmail.com>
2024-11-04 23:01:03 -0300

    Update

--- Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex115/lib/interface/__init__.py
+++ Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex115/lib/interface/__init__.py
@@ -1,3 +1,6 @@
+from lib.interface import *
+from lib.arquivo import *
+
 def leiaInt(msg):
     while True:
         try:
@@ -31,3 +34,33 @@ def menu(lista):
     print(linha())
     opc = int(input('\033[32mSua opção: \033[m'))
     return opc
+
+
+def lerArquivo(nome):
+    try:
+        a = open(nome, 'rt')
+    except:
+        print('Erro ao ler o arquivo!')
+    else:
+        cabeçalho('Pessoas Cadastradas')
+        for linha in a:
+            dado = linha.split(';')
+            dado[1] = dado[1].replace('\n', '')
+            print(f'{dado[0]:<30}{dado[1]:>3} anos')
+    finally:
+        a.close()
+
+
+def cadastrar(arq, nome = 'desconhecido', idade = 0):
+    try:
+        a = open(arq, 'at')
+    except:
+        print('Houve um erro na abertura do arquivo!')
+    else:
+        try:
+            a.write(f'{nome};{idade}\n')
+        except:
+            print('Houve um erro ao escrever os dados!')
+        else:
+            print(f'Novo registro de {nome} adicionado.')
+            a.close()
--- Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex115/sistema.py
+++ Exercícios/Desafios/Mundo 03 - Estruturas Compostas/ex115/sistema.py
@@ -1,12 +1,20 @@
 from lib.interface import *
+from lib.arquivo import *
 from time import sleep
+
+arq = 'cursoemvideo.txt'
+if not arquivoExiste(arq):
+    criarArquivo(arq)
 
 while True:
     resposta = menu(['Ver Pessoas Cadastradas', 'Cadastrar Novas Pessoas', 'Sair do Sistema'])
     if resposta == 1:
-        cabeçalho('Opção 1')
+        lerArquivo(arq)
     elif resposta == 2:
-        cabeçalho('Opção 2')
+        cabeçalho('Novo cadastro')
+        nome = str(input('Nome: '))
+        idade = leiaInt('Idade: ')
+        cadastrar(arq, nome, idade)
     elif resposta == 3:
         cabeçalho('Saindo do Sistema...')
         break
